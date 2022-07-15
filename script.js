8ca5805c43592b2f3616703479e94764d7e92ffa
AceBurgundy <samadriansabalo99@gmail.com>
2022-07-15 19:53:15 +0800

    fixed script for responsive functions

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ let players = document.querySelectorAll(".action")
 let background = document.querySelector(".background");
 let winner = document.querySelector(".winner")
 let retry = document.querySelector(".retry")
+let goalPosition = ((parseInt(window.getComputedStyle(goal).getPropertyValue('bottom'), 10)) - 128);
 
 // rolls dice
 function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+let newScore = "";
+
 // game function
-function runGame(number) {
+function runGame(number, boost) {
     let car = document.getElementById(`car-${number}`)
     let image = document.querySelector(`.img-${number}`)
-    let carBottom = parseInt(window.getComputedStyle(car).getPropertyValue('bottom'), 10)
+    let carBottom = parseInt(window.getComputedStyle(car).getPropertyValue('bottom'), { boost })
     let score = document.getElementById(`player-${number}-score`)
 
-    if (carBottom > 715) {
+    if (carBottom > goalPosition) {
         background.classList.toggle("active")
         number === 'one' ? winner.textContent = "player1" : winner.textContent = "player2"
     } else {
@@ -27,8 +30,11 @@ function runGame(number) {
         image.classList.add("active")
 
         car.style.bottom = newCarDistance + "px";
-        score.children[0].textContent = dice;
+        score.children[0].textContent = dice
+        newScore = dice;
     }
+
+    return newScore
 }
 
 retry.addEventListener("click", () => {
@@ -42,20 +48,31 @@ retry.addEventListener("click", () => {
 
 })
 
+// runs game on click
+players.forEach((player) => {
+    player.addEventListener("click", (event) => {
+        if (event.target.classList.contains("player1")) {
+            screen.width < 1200 ? runGame('one', 5) : runGame('one', 10)
+        } else {
+            screen.width < 1200 ? runGame('two', 5) : runGame('two', 10)
+        }
+    })
+})
+
 
 // runs game on click
 players.forEach((player) => {
     player.addEventListener("click", (event) => {
-        event.target.classList.contains("player1") ? runGame('one') : runGame('two')
+        event.target.classList.contains("player1") ? runGame('one', 10) : runGame('two', 10)
     })
 })
 
 // Runs game on keypress
 document.addEventListener("keypress", (event) => {
     if ((event.key) === "z") {
-        runGame('one')
+        runGame('one', 10)
     } else if ((event.key) === ".") {
-        runGame('two')
+        runGame('two', 10)
     }
 })
 
@@ -92,4 +109,26 @@ function adjustVolume(value) {
     } else {
         backgroundMusic.volume = 0
     }
+}
+
+if (screen.width < 884) {
+    let raceTrackHalfSide = document.querySelectorAll(".race-track-half");
+
+    raceTrackHalfSide.forEach((track) => {
+        track.addEventListener("click", (event) => {
+            if (event.target.classList.contains("player1")) {
+                newScore = runGame('one', 5)
+                document.querySelector(".mobile-score-one").innerHTML = newScore
+            } else if (event.target.classList.contains("player2")) {
+                newScore = runGame('two', 5)
+                document.querySelector(".mobile-score-two").innerHTML = newScore
+            }
+        })
+    })
+}
+
+if (screen.width < 1200) {
+    document.querySelectorAll(".action").forEach((button) => {
+        button.textContent = "GO"
+    })
 }
